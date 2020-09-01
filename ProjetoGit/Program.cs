e8bd04fff86292f6367ba3bdb27609414648451c
Raphael Scursoni <ra.scursoni@gmail.com>
2020-09-01 12:52:14 -0300

    Adicionado a pergunta como voce estah

--- ProjetoGit/Program.cs
+++ ProjetoGit/Program.cs
@@ -4,6 +4,7 @@ namespace ProjetoGit {
     class Program {
         static void Main(string[] args) {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Como você está?");
         }
     }
 }
